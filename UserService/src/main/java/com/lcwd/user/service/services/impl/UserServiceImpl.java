4a7e59af4f97820e8c341d2bc2b4f56466f05391
ParasJain12 <parasjain8103@gmail.com>
2025-08-13 02:21:35 +0530

    Implemented manually communicaion between User and Rating module

--- UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.lcwd.user.service.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.lcwd.user.service.exceptions.ResourceNotFoundException;
 import com.lcwd.user.service.model.User;
@@ -16,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 	
 	@Override
 	public User saveUser(User user) {
@@ -31,7 +40,10 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User getUser(String userId) {
-		return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id not found"));
+		User user =  userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id not found"));
+		ArrayList forObject = restTemplate.getForObject("http://localhost:8083/ratings/users/4e030b72-6324-4aa4-9488-d87f53b4791f",ArrayList.class);
+		logger.info("{} ",forObject);
+		return user;
 	}
 
 }
