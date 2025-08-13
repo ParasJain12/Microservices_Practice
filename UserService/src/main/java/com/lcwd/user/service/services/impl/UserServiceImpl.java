b3d81c1e7775beac01473c01e833cfae4871aab4
ParasJain12 <parasjain8103@gmail.com>
2025-08-13 18:50:09 +0530

    Calling two microservices together, do it hardcoded, i.e., manually

--- UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
@@ -1,16 +1,20 @@
 package com.lcwd.user.service.services.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.lcwd.user.service.exceptions.ResourceNotFoundException;
+import com.lcwd.user.service.model.Hotel;
 import com.lcwd.user.service.model.Rating;
 import com.lcwd.user.service.model.User;
 import com.lcwd.user.service.repository.UserRepository;
@@ -43,9 +47,19 @@ public class UserServiceImpl implements UserService {
 	public User getUser(String userId) {
 		User user =  userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id not found"));
 		//ArrayList<Rating> ratingsForUser = restTemplate.getForObject("http://localhost:8083/ratings/users/4e030b72-6324-4aa4-9488-d87f53b4791f",ArrayList.class);
-		ArrayList<Rating> ratingsForUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(),ArrayList.class);
+		Rating[] ratingsForUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(),Rating[].class);
 		logger.info("{} ",ratingsForUser);
-		user.setRatings(ratingsForUser);
+		
+		List<Rating> ratings = Arrays.stream(ratingsForUser).toList();
+		List<Rating> ratingList = ratings.stream().map(rating ->{
+			//http://localhost:8082/hotels/b5961cf7-55ee-4070-817c-36649a1fe013
+			ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+			Hotel hotel = forEntity.getBody();
+			logger.info("Response status code: {}",forEntity.getStatusCode());
+			rating.setHotel(hotel);
+			return rating;
+		}).collect(Collectors.toList());
+		user.setRatings(ratingList);
 		return user;
 	}
 
