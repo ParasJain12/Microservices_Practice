c83ea538336627e3417ff36532d1b71046a27c68
ParasJain12 <parasjain8103@gmail.com>
2025-08-19 01:53:51 +0530

    OKTA implementation in APIGateway Module but still gives error

---
+++ ApiGateway/src/main/java/com/lcwd/gateway/model/AuthResponse.java
@@ -0,0 +1,68 @@
+package com.lcwd.gateway.model;
+
+import java.util.Collection;
+
+public class AuthResponse {
+
+	private String userId;
+	private String accessToken;
+	private String refreshToken;
+	private long expireAt;
+	private Collection<String> authorities;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+
+	public long getExpireAt() {
+		return expireAt;
+	}
+
+	public void setExpireAt(long expireAt) {
+		this.expireAt = expireAt;
+	}
+
+	public Collection<String> getAuthories() {
+		return authorities;
+	}
+
+	public void setAuthories(Collection<String> authorities) {
+		this.authorities = authorities;
+	}
+
+	public AuthResponse(String userId, String accessToken, String refreshToken, long expireAt,
+			Collection<String> authorities) {
+		super();
+		this.userId = userId;
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+		this.expireAt = expireAt;
+		this.authorities = authorities;
+	}
+
+	public AuthResponse() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
